98cd8ff520455a19e906119218943ddb2732a390
venomqex <128019975+venomqex@users.noreply.github.com>
2024-12-31 12:58:32 +0300

    Update versions.h

--- Cloud Files/versions.h
+++ Cloud Files/versions.h
@@ -1,1 +1,8 @@
-const float Version = 5.0;
+//x
+
+#define FLAKE_VERSION "5.0" // Mevcut sürüm numarası
+#define RELEASE_DATE "2024-12-31" // Sürüm yayınlanma tarihi
+#define UPDATE_URL "https://raw.githubusercontent.com/venomqex/bad/refs/heads/main/Cloud%20Files/versions.h" // Güncelleme URL'si
+
+//
+#define RELEASE_NOTES "Update."
